192740485859eebd1539b8f84cb33881897dd328
Glodean David <glodean.david@yahoo.com>
2022-11-16 19:48:34 +0200

    no message

--- src/main/java/com/sda/David/bookmanagement/Main.java
+++ src/main/java/com/sda/David/bookmanagement/Main.java
@@ -1,7 +1,10 @@
 package com.sda.David.bookmanagement;
 
+import com.sda.David.bookmanagement.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        SessionManager.getSessionFactory();
     }
 }
--- src/main/java/com/sda/David/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/David/bookmanagement/utils/SessionManager.java
@@ -1,6 +1,9 @@
 package com.sda.David.bookmanagement.utils;
 
 
+import com.sda.David.bookmanagement.model.Author;
+import com.sda.David.bookmanagement.model.Book;
+import com.sda.David.bookmanagement.model.Review;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 public class SessionManager extends AbstractSessionManager {
@@ -21,6 +24,8 @@ public class SessionManager extends AbstractSessionManager {
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
         // Hibernate model will be added here
-
+configuration.addAnnotatedClass(Author.class);
+configuration.addAnnotatedClass(Book.class);
+configuration.addAnnotatedClass(Review.class);
     }
 }
