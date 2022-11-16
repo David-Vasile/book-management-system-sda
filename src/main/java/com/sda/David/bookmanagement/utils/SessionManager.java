347391217c5768d35190380682377b850bd6411d
Glodean David <glodean.david@yahoo.com>
2022-11-16 18:20:42 +0200

    no message

--- src/main/java/com/sda/David/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/David/bookmanagement/utils/SessionManager.java
@@ -1,2 +1,26 @@
-package com.sda.David.bookmanagement.utils;public class SessionManager {
+package com.sda.David.bookmanagement.utils;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class SessionManager extends AbstractSessionManager {
+
+    private static final SessionManager INSTANCE = new SessionManager();
+
+    private SessionManager() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return INSTANCE.getSessionFactory("book_manager_system");
+    }
+
+    public static void shutDown() {
+        INSTANCE.shutdownSessionManager();
+    }
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        // Hibernate model will be added here
+
+    }
 }
