b9ecbb1f04e67554e7508daef7031336a1bf15ab
Glodean David <glodean.david@yahoo.com>
2022-11-16 19:09:51 +0200

    As a user I should be able to view all the reviews for a given book title. In case book id doesn't exist in the system, the system will show an error message.

--- src/main/java/com/sda/David/bookmanagement/model/Author.java
+++ src/main/java/com/sda/David/bookmanagement/model/Author.java
@@ -1,2 +1,94 @@
-package com.sda.David.bookmanagement.model;public class Author {
+package com.sda.David.bookmanagement.model;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+   @ManyToOne
+   @JoinColumn(name = "author_id")
+    private Author author;
+   @OneToMany(mappedBy = "book")
+   private List<Review> reviews;
+
+
+
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+   // public List<Book> getBooks() {
+  //      return books;
+  //  }
+
+   // public void setBooks(List<Book> books) {
+   //     this.books = books;
+    //}
+
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/sda/David/bookmanagement/model/Review.java
+++ src/main/java/com/sda/David/bookmanagement/model/Review.java
@@ -1,2 +1,63 @@
-package com.sda.David.bookmanagement.model;public class Review {
+package com.sda.David.bookmanagement.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book_review")
+public class Review {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id")
+    private Integer id;
+    @Column(name = "score")
+
+    private Integer score;
+    @Column(name = "comment")
+
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "name_book")
+    private Book book;
+
+
+    public Review() {
+    }
+
+    public Review(Integer score, String comment) {
+        this.score = score;
+        this.comment = comment;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", score=" + score +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
